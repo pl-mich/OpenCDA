171510dc070b91445a24f25aec4d874b46ffe4c2
Peijing Li <peijli@umich.edu>
2022-11-02 06:09:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,9 @@ if [ -z "$CARLA_VERSION" ]; then
    CARLA_VERSION="0.9.11"
 fi
 
-CARLA_EGG_FILE=${CARLA_HOME}/PythonAPI/carla/dist/carla-"${CARLA_VERSION}"-py3.7-linux-x86_64.egg
+# CARLA_EGG_FILENAME=carla-"${CARLA_VERSION}"-py3.7-linux-x86_64.egg # use this for linux
+CARLA_EGG_FILENAME=carla-"${CARLA_VERSION}"-py3.7-win-amd64 # use this for windows
+CARLA_EGG_FILE=${CARLA_HOME}/PythonAPI/carla/dist/"${CARLA_EGG_FILENAME}".egg
 if [ ! -f "$CARLA_EGG_FILE" ]; then
     echo "Error: $CARLA_EGG_FILE can not be found. Please make sure you are using python3.7 and carla 0.9.11. "
     return 0
@@ -24,17 +26,17 @@ if [ ! -d "$CACHE" ]; then
 fi
 
 echo "copying egg file to cache folder"
-cp  $CARLA_EGG_FILE $CACHE
+cp  "$CARLA_EGG_FILE" "$CACHE"
 
 echo "unzip egg file"
-unzip "${CACHE}"/carla-"${CARLA_VERSION}"-py3.7-linux-x86_64.egg -d "${CACHE}"/carla-"${CARLA_VERSION}"-py3.7-linux-x86_64
+unzip "${CACHE}"/"${CARLA_EGG_FILENAME}".egg -d "${CACHE}"/"${CARLA_EGG_FILENAME}"
 
 echo "copy setup file to egg folder"
 SETUP_PY=${PWD}/scripts/setup.py
-cp "$SETUP_PY"  "${CACHE}"/carla-"${CARLA_VERSION}"-py3.7-linux-x86_64/
+cp "$SETUP_PY"  "${CACHE}"/"${CARLA_EGG_FILENAME}"
 
-echo "Successful! Run 'pip install -e ${CACHE}/carla-${CARLA_VERSION}-py3.7-linux-x86_64' to install carla into your python package "
+echo "Successful! Run 'pip install -e ${CACHE}/${CARLA_EGG_FILENAME}' to install carla into your python package "
 conda activate opencda
-pip install -e ${CACHE}/carla-"${CARLA_VERSION}"-py3.7-linux-x86_64
+pip install -e ${CACHE}/"${CARLA_EGG_FILENAME}"
 
 echo "Sucessful Setup!"
